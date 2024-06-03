ffd5104073d6b4ae0cf10de2ef4960a10b4f540c
isshoman123 <jaewk10@gmail.com>
2024-06-04 01:03:26 +0900

    yfinance first test code(nvidia,amd)
    yfinance로 엔비디아와 AMD의 주가 데이터를 가져와 간단하게 수익률 계산하는 코드를 작성해봤습니다.

---
+++ test-codes/Test,SampleCode.py
@@ -0,0 +1,40 @@
+# yfinance sample, test Code
+
+# pip install yfinance
+import yfinance as yf
+
+# first sample
+# 엔비디아 주식 데이터 다운로드
+nvidia = "NVDA"
+nvda = yf.Ticker(nvidia)
+
+# 최근 1년 및 5년간 주가 데이터 지정
+nvda_data_1y = nvda.history(period="1y")
+nvda_data_5y = nvda.history(period="5y")
+
+# 만약 1년전과 5년전에 투자했을 시, 현재 수익률
+# 수익률 계산 및 출력(%) 함수
+def calbenefit(data):
+    start_price = data['Close'][0]              # 시작 시점 종가 (데이터셋에서 Close 컬럼 첫번째, 인덱스 0)
+    end_price = data['Close'][-1]               # 현재 종가                             (마지막 인덱스 -1)
+    return_rate = ((end_price - start_price) / start_price) * 100
+    return print("수익률",return_rate,"%")
+
+# 엔비디아의 1년 및 5년 수익률 계산
+calbenefit(nvda_data_1y)
+
+calbenefit(nvda_data_5y)
+
+
+
+# AMD 주식 데이터를 다운로드
+
+amd = yf.Ticker("AMD")
+
+# 최근 1년 및 5년간의 주가 데이터 지정
+amd_data_1y = amd.history(period="1y")
+amd_data_5y = amd.history(period="5y")
+
+# AMD의 1년 및 5년 수익률 계산
+calbenefit(amd_data_1y)
+calbenefit(amd_data_5y)
